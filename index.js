5ab8675bc474610ec47ba548a62b68151f03b3ed
Kamath <niranjan-r.kamath@hpe.com>
2020-02-22 19:22:16 +0530

    initial set of files

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res) => {
+
+res.send('Hi There from Niranjan!');
+});
+
+app.listen(8080, () =>{
+
+console.log('Listening on port 8080');
+});
+
